68ffb3d098b24539216277fc7f96dda30fb27ea0
j-hippie-dev <j.hippie.dev@gmail.com>
2026-05-19 15:45:04 +0900

    260519 [develop] init

---
+++ src/main/java/com/msa4hipgram/Msa4HipgramApplication.java
@@ -0,0 +1,13 @@
+package com.msa4hipgram;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Msa4HipgramApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(Msa4HipgramApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/msa4hipgram/Msa4HipgramApplicationTests.java
@@ -0,0 +1,13 @@
+package com.msa4hipgram;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Msa4HipgramApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
